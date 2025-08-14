047236171ffca3d56ebeab005cab80f321859ab9
Gabriel Teixeira Silveira <gabriel.silveira.dev@gmail.com>
2025-08-14 14:39:17 -0300

    Correção da léxica e adicionei o main do lexer modified:   src/prolixa.sable modified:   src/prolixa/Main.java modified:   teste/teste.calc

--- src/prolixa/Main.java
+++ src/prolixa/Main.java
@@ -10,19 +10,23 @@ public class Main
  {
   try
   {
-   String arquivo = "teste/codigo.prlx";
+   String arquivo = "teste/teste.calc";
   
    Lexer lex = new Lexer(
 		    new PushbackReader(  
 		    new FileReader(arquivo), 1024));
+   Token token;
+   while (!((token = lex.next()) instanceof EOF)) {
+	   System.out.print(token.getClass());
+	   System.out.println('(' + token.toString() + ')');
+   }
+//   Parser p = new Parser(lex); 
    
-   Parser p = new Parser(lex); 
-   
-   Start tree = p.parse();
+//   Start tree = p.parse();
    //Imprime árvore na saída padrão
    //tree.apply(new ASTPrinter());
    //Imprime árvore em interface gráfica
-   tree.apply(new ASTDisplay());
+//   tree.apply(new ASTDisplay());
   }
   catch(Exception e)
   {
